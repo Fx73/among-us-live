37ccbdf6497286e2b38fb2e0f912dc88355e4929
Fx73 <fx73000@yahoo.fr>
2022-04-27 22:40:54 +0200

    Firebase config

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,13 @@ export const config = {
   gameCodeLength: 10,
 
 };
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAS3VVmc7DgmDzGRYukQDE31nXnosiT2r8',
+  authDomain: 'among-us-live-fe.firebaseapp.com',
+  projectId: 'among-us-live-fe',
+  storageBucket: 'among-us-live-fe.appspot.com',
+  messagingSenderId: '715830132861',
+  appId: '1:715830132861:web:1b7903f70489b1ccfff114',
+  measurementId: 'G-RD8QKW3N51'
+};
